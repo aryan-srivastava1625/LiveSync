eea254256934c7b28cf8e318ff49ab0729617d89
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-01-16 18:38:00 +0530

    done with the markers

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Socket.io connection
 io.on("connection", function(socket) {
-    console.log("connected");
+    socket.on("send-location", function(data){
+        io.emit('receive-location',{id: socket.id, ...data});
+    });
+
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected",socket.id);
+    });
 });
 
 // Routes
--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,23 @@
 const socket = io();
 console.log("hey");
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position)=>{
-        const {latitude , longitude } = position.coords;
-        socket.emit("send-location",{latitude , longitude});
+if(navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const {latitude, longitude} = position.coords;
+        socket.emit("send-location", {latitude, longitude});
     },
-    (error)=>{
+    (error) => {
         console.error(error);
     },
     {
         enableHighAccuracy: true,
-        timeout:5000,
-        maximumAge:0,
-
-    }
-);
+        timeout: 10000,  // Increased timeout to 10 seconds
+        maximumAge: 0,
+    });
 }
 
-
-
 // Initialize the map
-const map = L.map('map').setView([0,0], 10);
+const map = L.map('map').setView([0, 0], 16);
 
 // Add the tile layer (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -29,3 +25,22 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© LiveSync'
 }).addTo(map);
 
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+    
+    if(markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);  // Fixed method name
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    if(markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
