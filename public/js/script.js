7bf48217db878e902a701e48fc2d9b8d87a23f2a
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-01-16 18:04:46 +0530

    added the tile layer

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,31 @@
 const socket = io();
 console.log("hey");
 
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const {latitude , longitude } = position.coords;
+        socket.emit("send-location",{latitude , longitude});
+    },
+    (error)=>{
+        console.error(error);
+    },
+    {
+        enableHighAccuracy: true,
+        timeout:5000,
+        maximumAge:0,
+
+    }
+);
+}
+
+
+
 // Initialize the map
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([0,0], 10);
 
 // Add the tile layer (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap contributors'
+    attribution: '© LiveSync'
 }).addTo(map);
+
